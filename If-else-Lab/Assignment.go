72fbd780314ecbd56f6f0bec16a3cb4f772b61b0
Rahul-Chhabra-27 <chhabrarahul2707@gmail.com>
2024-03-08 14:45:57 +0530

    if-else lan added

---
+++ If-else-Lab/Assignment.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func gradeAssignment(score int) string {
+	var grade string = ""
+	// your code goes here..
+	return grade
+}
+func main() {
+	score := 10;
+	fmt.Printf(gradeAssignment(score));
+}
