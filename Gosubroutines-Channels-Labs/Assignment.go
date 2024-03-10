15e534b17a125aa65398c655c84237edc12ebd07
Rahul-Chhabra-27 <chhabrarahul2707@gmail.com>
2024-03-10 14:09:09 +0530

    readme updated

---
+++ Gosubroutines-Channels-Labs/Assignment.go
@@ -0,0 +1,31 @@
+package main
+
+import "net/http"
+
+func main() {
+
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	// channel for communication b/w subroutines.
+	//channel := make(chan string)
+
+	for _, link := range links {
+		// call makeRequest function and pass the individual
+		// link and channel as parameters and use concurrency.
+	}
+
+	/*
+		Print the messages sent by child subroutines...
+	*/
+}
+
+func makeRequest(link string, channel chan string) {
+	// _ , err := http.Get(link)
+	// Uncomment line 29 and complete the function.
+}
